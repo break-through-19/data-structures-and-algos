45cde8680624ea096337aaf5a4a0952c1e5d8615
break-through-19 <poorani4.official@gmail.com>
2026-02-27 18:10:25 -0800

    Update-programs-and-README

--- graph/single-source-shortest-path/bfs-01.py
+++ graph/single-source-shortest-path/bfs-01.py
@@ -1,2 +1,73 @@
 # https://www.geeksforgeeks.org/dsa/0-1-bfs-shortest-path-binary-graph/
 # Related problem: https://www.interviewbit.com/problems/min-cost-path/
+"""
+Can be used for both directed and undirected graph
+
+
+TC: O(|V| + |E|)
+SC: O(|V| + |E|)
+"""
+
+from collections import deque
+import sys
+
+
+def minDist(n, src, adj):
+    # Initialize distances to infinity
+    # visited array is not used and dist is treated as visited array
+    # The first time dist is set with a value other than infinity its considered visited
+    # You will not encounter a value lesser than that
+    dist = [sys.maxsize] * n
+    dist[src] = 0
+
+    # Use deque for 0-1 BFS
+    dq = deque()
+    dq.append(src)
+
+    while dq:
+        u = dq.popleft()
+
+        # Process all adjacent vertices
+        for edge in adj[u]:
+            v = edge[0]
+            weight = edge[1]
+
+            # If we can improve the distance
+            if dist[u] + weight < dist[v]:
+                dist[v] = dist[u] + weight
+
+                # If weight is 0, push to front (higher priority)
+                # If weight is 1, push to back (lower priority)
+                if weight == 0:
+                    dq.appendleft(v)
+                else:
+                    dq.append(v)
+
+    return dist
+
+
+def main():
+    n = 9
+    src = 0
+    edges = [
+        (0, 1, 0), (0, 7, 1), (1, 2, 1), (1, 7, 1),
+        (2, 3, 0), (2, 5, 0), (2, 8, 1), (3, 4, 1), (3, 5, 1),
+        (4, 5, 1), (5, 6, 1), (6, 7, 1), (7, 8, 1)
+    ]
+
+    # Create adjacency list representation of the graph
+    adj = [[] for _ in range(n)]
+    for u, v, w in edges:
+        adj[u].append((v, w))
+        adj[v].append((u, w))
+
+    res = minDist(n, src, adj)
+    for i in range(n):
+        print(res[i], end=' ')
+    print()
+
+
+if __name__ == "__main__":
+    main()
+
+
--- graph/single-source-shortest-path/shortest-path-faster-algo.py
+++ graph/single-source-shortest-path/shortest-path-faster-algo.py
@@ -1,1 +1,41 @@
+
+"""
 # https://www.geeksforgeeks.org/shortest-path-faster-algorithm/
+
+
+"""
+
+from collections import deque
+import sys
+
+"""
+adj[u] = [[v1, w1], [v2, w2].. [vn, wn]]
+"""
+def shortest_path_faster_algorithm(adj):
+
+    V = len(adj)
+
+    #Todo - SC: O(|V|)
+    queue = deque([])
+    inQueue = [False] * V
+    min_dist = [sys.maxsize] * V
+
+    queue.append(0)
+    inQueue[0] = True
+    min_dist[0] = 0
+
+    #Todo: Theoritically O(VE) is possible, but in most cases average time complexity is O(E)
+    while queue:
+        node = queue.popleft()
+        inQueue[node] = False
+
+        for v, w in adj[node]:
+            if min_dist[v] > min_dist[node] + w:
+                min_dist[v] = min_dist[node] + w
+
+                if not inQueue[v]:
+                    queue.append(v)
+                    inQueue[v] = True
+
+    return min_dist
+
